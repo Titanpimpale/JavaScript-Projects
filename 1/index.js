51a9285fcc70e6706d7aa394f77870ccd9db928e
Rohit Pimpale <109168200+Titanpimpale@users.noreply.github.com>
2024-08-21 16:01:58 +0530

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,16 @@
+const body = document.getElementsByTagName("body")[0]
+
+
+function color(name){
+    body.style.backgroundColor = name;
+}
+
+function random(){
+    const red =  Math.round(Math.random()*255);
+    const green =  Math.round(Math.random()*255);
+    const blue =  Math.round(Math.random()*255);
+
+    const  colour = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = colour;
+}
+random()
